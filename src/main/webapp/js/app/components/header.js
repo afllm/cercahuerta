941c83dd6b22ceb58b797b7eabc839ec11bab8fa
Alejandro Llamas <afllm@github.com>
2019-02-09 14:37:19 +0100

    evento scroll en componente header 09-02-2019

--- src/main/webapp/js/app/components/header.js
+++ src/main/webapp/js/app/components/header.js
@@ -7,7 +7,7 @@ moduloDirectivas.component('headerComponent', {
 });
 
 
-function js(toolService, sessionService, $http, $route) {
+function js(toolService, sessionService, $http, $route, $window) {
     var self = this;
 
 
@@ -17,6 +17,27 @@ function js(toolService, sessionService, $http, $route) {
     self.tipousuarioID = sessionService.getTypeUserID();
     self.isActive = toolService.isActive;
     self.limpiar = sessionService.isSessionActive();
-    
 
+    self.endOfDocumentTop = 134;
+    self.size = 0;
+
+     $window.onscroll = function () {
+         setTimeout(growShrinkLogo(), 2000);
+    };
+
+    // var Logo = document.getElementById("Logo");
+    var endOfDocumentTop = 134;
+    var size = 0;
+
+    function growShrinkLogo() {
+        var scroll = $window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+        if (size === 0 && scroll > endOfDocumentTop) {
+            document.getElementById("Logo").className = 'smallLogo';
+            size = 1;
+        } else if (size === 1 && scroll <= endOfDocumentTop) {
+            document.getElementById("Logo").className = 'largeLogo';
+            size = 0;
+        }
+    }
 }
